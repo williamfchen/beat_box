7324506d51b126588fd56783d3a74622597f989e
Will Chen <wfchen1990@gmail.com>
2023-07-06 17:41:49 -0600

    Finish nodes

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Node do
         
         expect(node).to be_an_instance_of(Node)
         expect(node.data).to eq("plop")
+        expect(node.next_node).to eq(nil)
     end
-    
+
 end
