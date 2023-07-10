32f6beebfca6f9218ed6b259457691aa1d8ca7e7
Will Chen <wfchen1990@gmail.com>
2023-07-10 12:46:22 -0600

    Add changeable voices

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -1,12 +1,13 @@
 class BeatBox
-    attr_accessor :list
-    attr_reader :approved_sounds, :rate
+    attr_accessor :list, :rate, :voice
+    attr_reader :approved_sounds 
 
-    def initialize(list, rate = 500)
+    def initialize(list, rate = 500, voice = "Boing")
         @list = LinkedList.new
         # list = append(list) creates approverd sound nil
         @approved_sounds = ["tee", "dee", "deep", "dop", "bop", "boop", "la", "na", "plop", "hi", "ho", "hee", "hum"]
         @rate = rate
+        @voice = voice
         append(list)
 
         # require 'pry';binding.pry
@@ -41,7 +42,7 @@ class BeatBox
     def play
         beats = list.to_string
         # require 'pry';binding.pry
-        `say -r #{rate} -v Ralph #{beats}`
+        system(`say -r #{rate} -v #{voice} #{beats}`)
     end
 
 
